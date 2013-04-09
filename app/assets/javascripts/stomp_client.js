85f42552c8b1a97029f5913692de8666626148cf
shruti-palshikar <shruti@buysidefx.com>
2013-04-09 15:36:13 -0400

    change ip in stomp client

--- app/assets/javascripts/stomp_client.js
+++ app/assets/javascripts/stomp_client.js
@@ -1,4 +1,4 @@
-client = Stomp.client( "ws://localhost:8675/" );
+client = Stomp.client( "ws://192.168.2.130:8675/" );
 client.connect( 'username', 'password', function() {
 });
   client.subscribe( '/queues/test_queue', function(message) {
